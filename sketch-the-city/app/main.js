6b27406433026424eab6c1cb5272ea5a757cc9ed
sdouglas89 <sdouglas89.sd@gmail.com>
2021-12-22 12:30:04 -0500

    Update main.js

--- sketch-the-city/app/main.js
+++ sketch-the-city/app/main.js
@@ -72,7 +72,7 @@ require([
   function setSketchRenderer(layer) {
 
     const outlineColor = mode === "dark" ? [232, 176, 15, 0.8] : [0, 0, 0, 0.8];
-    const fillColor = mode === "dark" ? [10, 10, 10, 0.1] : [255, 255, 255, 0.1];
+    const fillColor = mode === "dark" ? [10, 10, 10, 0.1] : [232, 176, 15, 0.1];
     const size = mode === "dark" ? 2 : 1;
 
     const sketchEdges = {
